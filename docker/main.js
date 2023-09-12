ac302f2171c73161620d601bf2eed471620c9997
Leon-Africa <mr.leon.africa@gmail.com>
2023-09-12 07:18:22 +0200

    docker

---
+++ docker/main.js
@@ -0,0 +1,20 @@
+var http = require("http");
+require('dotenv').config();
+
+http.createServer(function (request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end(`
+     ________________________________________
+    < mooooooooooooooooooooooooooooooooooooo >
+     ----------------------------------------
+           \\
+            \\   ^__^
+             \\  (oo)\\_______
+                (__)\\       )\\/\\
+                    ||----w |
+                    ||     ||
+    `);
+}).listen(8080);
+
+console.log('Server running at http://127.0.0.1:8080/');
+console.log(process.env.FOOBAR);
